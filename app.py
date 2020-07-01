4eb432b3740d3883268182e5dbfb765586c5b49c
Boris Iskra <borisiskra@gmail.com>
2020-07-01 10:01:26 -0400

    add graph function

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+@app.route('/graph')
+def graph(SYMBOL):
+    API_KEY = '7ENJ7TOOUOLNE7W4'
+    #Welcome to Alpha Vantage! Here is your API key: 7ENJ7TOOUOLNE7W4. Please record this API key for future access to Alpha Vantage.
+    #https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=demo
+        SYMBOL = request.form['ticker']
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=' + SYMBOL + '&apikey=' + API_KEY
+
+    
+    return url
+    return render_template('about.html')
+
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
